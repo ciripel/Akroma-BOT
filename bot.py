5e4172f8274d097054d8d47d828e5c8b18cc4eef
ciripel <nedelcu.alexandru@yahoo.com>
2018-10-06 19:37:24 +0300

    v.1.5 - basic start - easyest function implemented Made the initial setup for python

--- bot.py
+++ bot.py
@@ -1,29 +1,58 @@
 # Work with Python 3.6
 
-import discord
 import json
 
-with open('auth.json') as data_file:
+import discord
+from discord.ext.commands import Bot
+
+with open("auth.json") as data_file:
+    auth = json.load(data_file)
+with open("links.json") as data_file:
     data = json.load(data_file)
 
-TOKEN = data['token']
+TOKEN = auth["token"]
+BOT_PREFIX = "?"
 
-client = discord.Client()
+client = Bot(BOT_PREFIX)
 
 
 @client.event
-async def on_message(message):
+async def on_message(msg):
+    m = msg.content.lower()
+    check = (
+        msg.channel.name == "akroma-bot"
+        or msg.channel.type == discord.ChannelType.private
+        or "Core-Team" in [role.name for role in msg.author.roles]
+        or "Support-Team" in [role.name for role in msg.author.roles]
+        or "Contributors" in [role.name for role in msg.author.roles]
+    )
     # we do not want the bot to reply to itself
-    if message.author == client.user:
+    if msg.author == client.user:
         return
-
-    if message.content.startswith('!'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+    if check:
+        if m.startswith("?help"):
+            message = "\n".join(data["help"])
+            await client.send_message(msg.channel, message)
+        elif m.startswith("?links"):
+            message = "\n".join(data["links"])
+            await client.send_message(msg.channel, message)
+        elif m.startswith("?roadmap"):
+            message = f"{data['roadmap']}"
+            await client.send_message(msg.channel, message)
+        elif m.startswith("?awesome"):
+            message = f"{data['awesome']}"
+            await client.send_message(msg.channel, message)
+        elif m.startswith("?about"):
+            message = "\n".join(data["about"])
+            await client.send_message(msg.channel, message)
+    else:
+        message = f"{data['default']}"
+        await client.send_message(msg.channel, message)
 
 
 @client.event
 async def on_ready():
-    print('Logged in as: {0}'.format(client.user.name))
+    print(f"Logged in as: {client.user.name} {{{client.user.id}}}")
+
 
 client.run(TOKEN)
